238e77af62647086045bf07500e11ff51aae379f
Kushal Vadodaria <kushalv238@gmail.com>
2022-04-26 16:45:56 +0530

    commit 1

---
+++ script/script.js
@@ -0,0 +1,35 @@
+var monthName = ["Janaury", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dayName = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var date_ = new Date();
+
+function textFill() {
+    document.getElementById("date").value = `${date_.getDate()}`
+    document.getElementById("mo").textContent = `${monthName[date_.getMonth()]}`
+    document.getElementById("year").value = `${date_.getFullYear()}`
+}
+
+function getDay() {
+    date_.setDate(parseInt((document.getElementById("date") || {}).value || "0"));
+    date_.setMonth(getMonthNumber((document.getElementById("month") || {}).value || "0"));
+    date_.setFullYear(parseInt((document.getElementById("year") || {}).value || "0"));
+    
+    console.log(date_);
+
+    var day = getDayName(date_.getDay());
+    console.log(day);
+
+    document.getElementById("enterDay").textContent = `${day}`
+}
+        
+function getMonthNumber(Month) {
+    for(var i = 0; i < 11; i++) {
+        if(monthName[i] == Month) {
+            return i;
+        }
+    }
+}
+
+function getDayName(dayNo) {
+    return dayName[dayNo];
+}
